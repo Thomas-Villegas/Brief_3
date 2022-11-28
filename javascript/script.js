1bf81100fef457c1fae8683201847fcc52c0d269
Thomas-Villegas <tomvillegas2490@gmail.com>
2022-11-28 16:11:25 +0100

    NICE JOB 🥳
    Brief Fini

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,68 @@
+
+/* Apparition des blocs */
+
+const blockAdvent1 = document.querySelector('.section2__text');
+
 window.addEventListener('scroll', () => {
-    console.log('scroll!');
+    const {scrollTop, clientHeight} = document.documentElement;
+    const topElementToTopViewport = blockAdvent1.getBoundingClientRect().top;
+    
+    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.80) {
+        blockAdvent1.classList.add('active')
+    }
 })
 
-/* const blockAdvent1 = document.querySelector('.section2__text'); */
-/* const blockAdvent2 = document.querySelector('.section2__cards1'); */
-/* const blockAdvent3 = document.querySelector('.section4__frontend'); */
-/* const blockAdvent4 = document.querySelector('.section4__backend'); */
-/* const blockAdvent5 = document.querySelector('.section4__system'); */
+const blockAdvent2 = document.querySelector('.section4__frontend');
 
-
-/* window.addEventListener('scroll', () => {
-    if(window.scrollY > 880 ) {
-        blockAdvent1.classList.add('scroll');
+window.addEventListener('scroll', () => {
+    const {scrollTop, clientHeight} = document.documentElement;
+    const topElementToTopViewport = blockAdvent2.getBoundingClientRect().top;
+    
+    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.80) {
+        blockAdvent2.classList.add('active')
     }
-}); */
+})
 
-/* window.addEventListener('scroll', () => {
-    if(window.scrollY > 1300 ) {
-        blockAdvent2.classList.add('scroll');
-    }
-}); */
+const blockAdvent3 = document.querySelector('.section4__backend');
 
-/* window.addEventListener('scroll', () => {
-    if(window.scrollY > 2255 ) {
-        blockAdvent3.classList.add('scroll');
+window.addEventListener('scroll', () => {
+    const {scrollTop, clientHeight} = document.documentElement;
+    const topElementToTopViewport = blockAdvent3.getBoundingClientRect().top;
+    
+    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.80) {
+        blockAdvent3.classList.add('active')
     }
-}); */
+})
 
-/* window.addEventListener('scroll', () => {
-    if(window.scrollY > 2695 ) {
-        blockAdvent4.classList.add('scroll');
-    }
-}); */
+const blockAdvent4 = document.querySelector('.section4__system');
 
-/* window.addEventListener('scroll', () => {
-    if(window.scrollY > 2840 ) {
-        blockAdvent5.classList.add('scroll');
+window.addEventListener('scroll', () => {
+    const {scrollTop, clientHeight} = document.documentElement;
+    const topElementToTopViewport = blockAdvent4.getBoundingClientRect().top;
+    
+    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.80) {
+        blockAdvent4.classList.add('active')
     }
-}); */
+})
+
+
+/* Evenement rotation cards */
+const ratio = 0.2
+const option = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+        entry.target.classList.add('active')
+    } else {
+        entry.target.classList.remove('active')
+    } 
+    })
+}
+const observer = new IntersectionObserver(handleIntersect, option)
+document.querySelectorAll('.cardsall').forEach(function (r) {
+    observer.observe(r)
+})
