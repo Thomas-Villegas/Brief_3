4c0a77549e10560e97ae045be89055586a612291
Thomas-Villegas <tomvillegas2490@gmail.com>
2022-11-29 16:12:00 +0100

    test formulaire 2

--- index.php
+++ index.php
@@ -528,6 +528,24 @@
                 </div>
             </footer>
         </div>
+        <?php
+            if (isset($_POST("submit"))){
+                $nom = $_POST["lastNameForm"];
+                $prenom = $_POST["firstNameForm"];
+                $email = $_POST["emailForm"];
+                $objet = $_POST["motifForm"];
+                $message = $_POST["textAreaForm"];
+
+                $to = "tomvillegas2490@gmail.com";
+                $headers = "From:" . $nom . " " . $prenom . " " . $email;
+
+                if (mail($to,$objet,$message,$headers)){
+                    echo " Ton message est bien envoyé.";
+                } else {
+                    echo "Une erreur s'est produite";
+                }
+            }
+        ?>
         <script src="javascript/script.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
