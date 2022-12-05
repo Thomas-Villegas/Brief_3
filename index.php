ec2eb2ebb381f668c99a29f067c386e261e555b1
Thomas-Villegas <tomvillegas2490@gmail.com>
2022-12-05 14:57:02 +0100

    test php

--- index.php
+++ index.php
@@ -19,6 +19,9 @@
 
     <body>
 
+        <?php 
+        echo "bonjour";
+        ?>
 <!---- MOBILE VERSION ---->
         <div id="content__mobile">
     <!---- HEADER MOBILE---->
@@ -478,6 +481,7 @@
                             <label for="emailFormDesktop" class="form-label">E-Mail *</label>
                             <input type="email" name="emailForm" class="form-control" id="emailFormDesktop" placeholder="marco@polo.com" required>
                         </div>
+
                         <div class="mb-5 col-6">
                             <label for="textAreaFormDesktop" class="form-label">Message *</label>
                             <textarea class="form-control" name="textAreaForm" id="textAreaFormDesktop" rows="6" placeholder="Écrivez votre message..." required></textarea>
@@ -529,8 +533,7 @@
             </footer>
         </div>
 
-        <?php phpinfo(); ?>
-/*         <?php
+<!--    <?php
             if (isset($_POST("submit"))){
                 $nom = $_POST["lastNameForm"];
                 $prenom = $_POST["firstNameForm"];
@@ -547,8 +550,8 @@
                     echo "Une erreur s'est produite";
                 }
             }
-        ?>
- */        <script src="javascript/script.js"></script>
+        ?>--> 
+       <script src="javascript/script.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
             crossorigin="anonymous"></script>
