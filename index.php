6a17735689a412099a1e5e2710e050377f60eaaa
Thomas-Villegas <tomvillegas2490@gmail.com>
2022-12-05 14:54:41 +0100

    test php

--- index.php
+++ index.php
@@ -528,7 +528,9 @@
                 </div>
             </footer>
         </div>
-        <?php
+
+        <?php phpinfo(); ?>
+/*         <?php
             if (isset($_POST("submit"))){
                 $nom = $_POST["lastNameForm"];
                 $prenom = $_POST["firstNameForm"];
@@ -546,7 +548,7 @@
                 }
             }
         ?>
-        <script src="javascript/script.js"></script>
+ */        <script src="javascript/script.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
             crossorigin="anonymous"></script>
